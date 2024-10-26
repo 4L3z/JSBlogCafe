56ce1b68b7b813cf0c4468a7145baffeadff1c77
4L3z <palaciosalejandro735@gmail.com>
2024-10-25 21:06:39 -0300

    Clase N4

--- js/app.js
+++ js/app.js
@@ -1,47 +1,60 @@
-// console.log("Hola Mundo");
+/* console.log("Hola")
 
+let nombre = "Nodo Tecnologico";
 
+let edad = 18 
 
+console.log("Hola soy " + nombre + "y tengo" + edad + "años")
 
-
-// let nombre = "Nodo Tecnologico";
-
-// let edad = 18;
-
-// console.log("Hola soy " + nombre + "y tengo " + edad + " años");
-
-// // Definicion de una funcion
-
-// function suma(numero1, numero2){
-//  return numero1 + numero2
-// }
-
-// // Llamado a la funcion
-
-// let resultado = suma(Infinity, Infinity)
-
-// console.log(resultado)
-
-
-const persona = {
-    nombre: "Alejandro Palacios",
-    edad: 17,
-    ageDeNacimiento: 2007,
-
+// Definicion de una función 
+function suma(numero1 , numero2){
+    return numero1 + numero2
 }
 
-console.log("Hola soy " + persona.nombre + ", tengo " + persona.edad + " de edad y naci en el " + persona.ageDeNacimiento)
+//Llamado a la función
 
-const textoHeader = document.querySelector("#heading")
+let resultado = suma("5","8")
 
-console.log(textoHeader)
+console.log(resultado) */
 
-textoHeader.textContent = "Node.js, React, JavaScript, CSS, HTML 5, Bar"
+const persona = {
+  nombre: "Nodo Tecnologico",
+  edad: 5,
+  anioDeNacimiento: 2020,
+};
 
-textoHeader.classList.add('nueva-clase')
+console.log(" hola soy el " + persona.edad);
 
-const enlaces = document.querySelectorAll('.navegacion a')
+// const textoHeader = document.querySelector("#heading");
 
-console.log(enlaces[0])
+// console.log(textoHeader);
 
-enlaces[0].textContent = "hola ajskldjklsajdkl"
+// textoHeader.textContent = "Nuevo Texto";
+
+// textoHeader.classList.add("nueva-clase");
+
+const enlaces = document.querySelectorAll(".navegacion a");
+
+console.log(enlaces[0]);
+
+enlaces[1].textContent = "Nuevo enlace";
+
+const nuevoEnlace = document.createElement("A");
+nuevoEnlace.textContent = "Ver Mas";
+nuevoEnlace.href = "https://paletadecolores.com.ar/";
+nuevoEnlace.classList.add("nueva-clase");
+
+const navegacion = document.querySelector(".navegacion");
+
+navegacion.appendChild(nuevoEnlace);
+
+console.log(navegacion);
+
+const parrafoLindo = document.createElement("p");
+parrafoLindo.textContent =
+  "La va a tocar para Diego, ahí la tiene Maradona, lo marcan dos, pisa la pelota Maradona, arranca por la derecha el genio del fútbol mundial, deja el tendal y va a tocar para Burruchaga… ¡Siempre Maradona! ¡Genio! ¡Genio! ¡Genio! Ta-ta-ta-ta-ta-ta-ta-ta… Gooooool… Gooooool… ¡Quiero llorar! ¡Dios Santo, viva el fútbol! ¡Golaaazooo! ¡Diegoooool! ¡Maradona! Es para llorar, perdónenme… Maradona, en recorrida memorable, en la jugada de todos los tiempos… Barrilete cósmico… ¿De qué planeta viniste para dejar en el camino a tanto inglés, para que el país sea un puño apretado gritando por Argentina? Argentina 2 – Inglaterra 0. Diegol, Diegol, Diego Armando Maradona… Gracias, Dios, por el fútbol, por Maradona, por estas lágrimas, por este Argentina 2-Inglaterra 0.";
+parrafoLindo.classList.add('parrafoHermoso')
+
+const TextoHeader = document.querySelector(".header__texto");
+
+TextoHeader.appendChild(parrafoLindo);
