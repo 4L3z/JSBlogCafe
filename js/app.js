f985409f346434b368730a25d13808e860713f4d
4L3z <palaciosalejandro735@gmail.com>
2024-10-30 20:53:09 -0300

    hola gente

--- js/app.js
+++ js/app.js
@@ -52,9 +52,35 @@ console.log(navegacion);
 
 const parrafoLindo = document.createElement("p");
 parrafoLindo.textContent =
-  "La va a tocar para Diego, ahí la tiene Maradona, lo marcan dos, pisa la pelota Maradona, arranca por la derecha el genio del fútbol mundial, deja el tendal y va a tocar para Burruchaga… ¡Siempre Maradona! ¡Genio! ¡Genio! ¡Genio! Ta-ta-ta-ta-ta-ta-ta-ta… Gooooool… Gooooool… ¡Quiero llorar! ¡Dios Santo, viva el fútbol! ¡Golaaazooo! ¡Diegoooool! ¡Maradona! Es para llorar, perdónenme… Maradona, en recorrida memorable, en la jugada de todos los tiempos… Barrilete cósmico… ¿De qué planeta viniste para dejar en el camino a tanto inglés, para que el país sea un puño apretado gritando por Argentina? Argentina 2 – Inglaterra 0. Diegol, Diegol, Diego Armando Maradona… Gracias, Dios, por el fútbol, por Maradona, por estas lágrimas, por este Argentina 2-Inglaterra 0.";
-parrafoLindo.classList.add('parrafoHermoso')
+  "Hola Gente";
+parrafoLindo.classList.add("parrafoHermoso");
 
 const TextoHeader = document.querySelector(".header__texto");
 
 TextoHeader.appendChild(parrafoLindo);
+
+const btnForm = document.querySelector('.formulario input[type=submit]')
+console.log(btnForm)
+
+
+function imprimirClick(e) {
+  e.preventDefault()
+  console.log("Hiciste Click");
+}
+
+btnForm.addEventListener('click', imprimirClick);
+
+
+const ContactoPalabra = document.querySelector('#palabraContacto')
+ContactoPalabra.classList.add("textohermosoxD")
+console.log(ContactoPalabra)
+
+
+function holaAmigos(){
+  alert("Realizaste Click")
+  ContactoPalabra.classList.toggle("textohermosoxD")
+}
+
+
+
+ContactoPalabra.addEventListener('click', holaAmigos)
